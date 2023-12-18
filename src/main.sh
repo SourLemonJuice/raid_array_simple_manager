18ad1991a46d4c6493394d25beebd763b72b9949
SourLemonJuice <106436955+SourLemonJuice@users.noreply.github.com>
2023-12-19 05:38:33 +0800

    修复bug和添加阵列组合与挂载功能 修复了取消挂载前的提示中无法取消的问题 修复无法待机硬盘的问题(因为根本就没写全啦) 添加快速组合阵列与挂载的逻辑

--- src/main.sh
+++ src/main.sh
@@ -18,30 +18,35 @@ case $1 in
         mdadm -D $Raid_Name
     ;;
     umount)
-        # 为了不让 input 变量留到下面，所以在子shell里运行
-        (
-            read -t 5 -p '这项操作可能非常危险，要确定吗[y/n]' input
-            llib_errored_exit
-            case $input in
-                y | Y)
-                    :
-                ;;
-                *)
-                    echo '操作取消'
-                    exit
-                ;;
-            esac
-        )
+        read -t 5 -p '这项操作可能非常危险，要确定吗[y/n]' input
+        llib_errored_exit
+        case $input in
+            y | Y)
+                :
+            ;;
+            *)
+                echo '操作取消'
+                exit
+            ;;
+        esac
 
         echo '即将卸载文件系统'
         umount $Raid_Name
+        echo '卸载命令执行结束'
+        sleep 4
         echo '即将关闭阵列'
         mdadm --stop $Raid_Name
+        echo '关闭命令执行结束'
+        sleep 4
         echo "即将 待机设备 ${Raid_Devices[@]}"
-        hdparm -y
+        hdparm -y ${Raid_Devices[@]}
     ;;
     check)
         echo idle > /sys/block/md0/md/sync_action
+    ;;
+    mount)
+        mdadm --assemble $Raid_Name
+        mount $Raid_Name
     ;;
     *)
         echo '非法参数'
